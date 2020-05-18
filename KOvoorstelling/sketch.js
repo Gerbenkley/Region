8ed113cd05aeb037617cb85938f7e460055537d7
Gerbenkley <65512404+Gerbenkley@users.noreply.github.com>
2020-05-18 17:38:56 +0200

    Update sketch.js

--- KOvoorstelling/sketch.js
+++ KOvoorstelling/sketch.js
@@ -8,6 +8,7 @@ function preload() {
   soundFormats('mp3', 'wav');
   arctic = loadSound('Arctic.mp3');
   ijs = loadSound('Ijs.mp3');
+  desert2 = loadSound('DesertSounds2.mp3');
 }
 
 
@@ -46,6 +47,8 @@ function drawBackground() {
           arctic.play();
           arctic.setVolume(0.3);
         }
+        desert2.setVolume(0, 0.5);
+        desert2.pause(1);
       } else if (mouseX > (windowWidth/3)*2) {
         background(225, mouseX/8, 35);
         ijs.setVolume(0, 0.5);
@@ -54,10 +57,16 @@ function drawBackground() {
         arctic.pause(1);
         mx.length = 0;
         my.length = 0;
+        if (desert2.isPlaying() == false) {
+          desert2.play();
+          desert2.setVolume(0.3);
+        }
       } else {
         background(35, mouseX/4, 130);
         ijs.setVolume(0, 0.5);
         ijs.pause(1);
+        desert2.setVolume(0, 0.5);
+        desert2.pause(1);
         arctic.setVolume(0, 0.5);
         arctic.pause(1);
         mx.length = 0;
