b4b341b85480724502942975eb03959fcecac38d
Gerbenkley <65512404+Gerbenkley@users.noreply.github.com>
2023-09-19 09:27:48 +0200

    Add files via upload

---
+++ Strings-rnbo/strings.js
@@ -0,0 +1,56 @@
+let points = [];
+
+
+function setup() {
+    canvas = createCanvas(windowWidth,windowHeight);
+    //background(242, 222, 179, 100);
+}
+
+function draw() { 
+    background(252, 240, 215, 100);
+
+    for (let i = points.length-1; i >= 0; i--) {
+        points[i].show();
+    }
+
+    if (points.length == 2) {
+        stroke(0);
+        strokeWeight(2);
+        line(points[0].x, points[0].y, points[1].x, points[1].y);
+    }
+}
+
+
+
+
+function mousePressed() {
+    if (points.length == 2) {
+        points = [];
+    }
+
+    points.push(new Point(mouseX, mouseY, 0, 100, 0.8));
+
+
+}
+
+
+class Point {
+    constructor(x, y, r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+
+    show() {
+        stroke(0);
+        strokeWeight(10);
+        ellipse(this.x, this.y, this.r);
+    }
+}
+
+
+
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
