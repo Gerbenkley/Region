931706dd3b8d48191369e8e3ce318197a1b2a336
Gerbenkley <65512404+Gerbenkley@users.noreply.github.com>
2023-09-19 15:19:06 +0200

    Add files via upload

--- Strings-rnbo/strings.js
+++ Strings-rnbo/strings.js
@@ -1,9 +1,44 @@
 let points = [];
 let lineLength;
+let audioContext;
+let device;
 
-function setup() {
+async function setup() {
     canvas = createCanvas(windowWidth,windowHeight);
     //background(242, 222, 179, 100);
+    console.log("Setup werkt");
+
+
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+    canvas.mouseClicked(startAudioContext);
+
+    audioContext.resume().then(() => {
+        console.log('playback resumed successfully');
+    });
+    loadRNBO();
+}
+
+async function loadRNBO() {
+    const { createDevice } = RNBO;
+    await audioContext.resume();
+    const rawPatcher = await fetch('strings.export.json');
+    const patcher = await rawPatcher.json();
+    device = await createDevice({ context: audioContext, patcher});
+    device.node.connect(audioContext.destination);
+
+    device.messageEvent.subscribe((ev) => {
+        if (ev.tag === "scaled") {
+          console.log("scaled");
+        }
+});
+
+}
+
+function startAudioContext() {
+    if (audioContext.state === 'suspended') {
+      audioContext.resume();
+    }
 }
 
 function draw() { 
@@ -39,20 +74,24 @@ function mousePressed() {
 function lengthLine() {
     lineLength = dist(points[0].x, points[0].y, points[1].x, points[1].y);
     console.log(lineLength);
+
+    const { TimeNow, MessageEvent } = RNBO;
+    const event1 = new MessageEvent(TimeNow, 'in1', [lineLength]);
+    device.scheduleEvent(event1);
+
 }
 
 
 class Point {
-    constructor(x, y, r) {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.r = r;
     }
 
     show() {
         stroke(0);
         strokeWeight(10);
-        ellipse(this.x, this.y, this.r);
+        ellipse(this.x, this.y, 1);
     }
 }
 
